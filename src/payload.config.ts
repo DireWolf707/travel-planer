71a376572f91e4314489dfbbd2fb37180aa39aaf
DireWolf707 <rahulsood707@gmail.com>
2023-04-17 20:04:57 +0530

    initial commit

---
+++ src/payload.config.ts
@@ -0,0 +1,20 @@
+import { buildConfig } from 'payload/config';
+import path from 'path';
+import Users from './collections/Users';
+
+export default buildConfig({
+  serverURL: process.env.SERVER_URL,
+  admin: {
+    user: Users.slug,
+  },
+  collections: [Users],
+  csrf: [String(process.env.CLIENT_URL)],
+  cors: [String(process.env.CLIENT_URL)],
+  cookiePrefix: "auth",
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+  },
+});
