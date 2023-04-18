0f63bd0b4c3e800726675ac8838e25bdd782be7e
DireWolf707 <rahulsood707@gmail.com>
2023-04-18 17:48:44 +0530

    added access and api

--- src/collections/Posts.ts
+++ src/collections/Posts.ts
@@ -1,9 +1,18 @@
 import { CollectionConfig } from "payload/types"
+import { isAdmin, isAuthenticated } from "../access"
+import FormData from "form-data"
+import axios from "axios"
 
 const Posts: CollectionConfig = {
   slug: "posts",
   admin: {
     group: "Social Media",
+  },
+  access: {
+    create: isAuthenticated,
+    read: isAuthenticated,
+    update: isAdmin,
+    delete: isAdmin,
   },
   upload: {
     staticURL: "/media",
@@ -31,8 +40,39 @@ const Posts: CollectionConfig = {
           type: "text",
         }
       ],
+      access: {
+        create: () => false,
+        read: () => true,
+        update: () => false,
+      }
     },
   ],
+  hooks: {
+    beforeChange: [async ({ data, req }) => {
+      if (!isAdmin({ req })) data = { ...data, userId: req.user.id }
+
+      const file = req?.files?.file
+      if (file) {
+        const formData = new FormData()
+        formData.append("image", file.data)
+
+        try {
+          const resp = await axios.post(
+            "https://api.imagga.com/v2/tags?limit=3&threshold=40.0",
+            formData,
+            { headers: { Authorization: process.env.IMAGGA_AUTHORIZATION_HEADER, "Content-Type": "multipart/form-data" } }
+          )
+          const tags = resp.data.result.tags.map((t) => ({ tag: t.tag.en }))
+          data = { ...data, tags }
+        } catch (err) {
+          console.log(err.message)
+          throw new Error("something went wrong!")
+        }
+      }
+
+      return data
+    }],
+  },
 }
 
 export default Posts
