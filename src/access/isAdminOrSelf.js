d11dc4a672a52b8ae18f18399d45038348901327
DireWolf707 <rahulsood707@gmail.com>
2023-04-18 13:13:35 +0530

    added access

---
+++ src/access/isAdminOrSelf.js
@@ -0,0 +1,9 @@
+import { isAdmin } from "./isAdmin"
+
+export default ({ req }) => {
+  if (!req?.user) return false
+
+  if (isAdmin({ req })) return true
+
+  return { id: { equals: req.user.id } }
+}
---
+++ src/access/isAuthenticated.js
@@ -0,0 +1,1 @@
+export default ({ req }) => Boolean(req?.user)
