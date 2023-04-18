7191e70c4191a66447f5b27396461f83e933a5d3
DireWolf707 <rahulsood707@gmail.com>
2023-04-18 15:14:01 +0530

    added access control and api

--- src/collections/Likes.ts
+++ src/collections/Likes.ts
@@ -1,9 +1,16 @@
 import { CollectionConfig } from "payload/types"
+import { isAdmin, isAuthenticated } from "../access"
 
 const Likes: CollectionConfig = {
   slug: "likes",
   admin: {
     group: "Social Media",
+  },
+  access: {
+    create: isAuthenticated,
+    read: isAuthenticated,
+    update: isAdmin,
+    delete: isAdmin,
   },
   fields: [
     {
@@ -19,6 +26,34 @@ const Likes: CollectionConfig = {
       required: true,
     },
   ],
+  hooks: {
+    beforeChange: [async ({ data, req }) => {
+      if (!isAdmin({ req })) data = { userId: req.user.id, postId: data.postId }
+
+      if (!data.postId) throw new Error("postId field not found")
+
+      const like = await req.payload.collections["likes"].Model.findOne(data)
+      if (like) throw new Error("Already liked!")
+
+      return data
+    }]
+  },
+  endpoints: [
+    {
+      path: "/unlike",
+      method: "post",
+      handler: [async (req, res, next) => {
+        const userId = req.user?.id
+        const postId = req.body?.postId
+
+        if (!userId) return next(new Error("not authenticated"))
+        if (!postId) return next(new Error("postId field not found"))
+
+        await req.payload.collections["likes"].Model.deleteOne({ userId, postId })
+        res.status(200).end()
+      }]
+    }
+  ]
 }
 
 export default Likes  
