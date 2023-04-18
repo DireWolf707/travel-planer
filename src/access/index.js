aee0955874576d21a392702ecdf9b4aadc23bce4
DireWolf707 <rahulsood707@gmail.com>
2023-04-18 13:19:12 +0530

    added index file

---
+++ src/access/index.js
@@ -0,0 +1,6 @@
+import isAdmin from "./isAdmin"
+import isAdminOrSelf from "./isAdminOrSelf"
+import isAnyone from "./isAnyone"
+import isAuthenticated from "./isAuthenticated"
+
+export { isAdmin, isAdminOrSelf, isAnyone, isAuthenticated }
