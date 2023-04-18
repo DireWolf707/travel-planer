3f6df15c455fe2f95298771155312bac0b000763
DireWolf707 <rahulsood707@gmail.com>
2023-04-18 15:00:31 +0530

    added access control and api

--- src/collections/Follows.ts
+++ src/collections/Follows.ts
@@ -1,9 +1,16 @@
 import { CollectionConfig } from "payload/types"
+import { isAdmin, isAuthenticated } from "../access"
 
 const Follows: CollectionConfig = {
   slug: "follows",
   admin: {
     group: "Social Media",
+  },
+  access: {
+    create: isAuthenticated,
+    read: isAuthenticated,
+    update: isAdmin,
+    delete: isAdmin,
   },
   fields: [
     {
@@ -20,10 +27,34 @@ const Follows: CollectionConfig = {
     },
   ],
   hooks: {
-    beforeChange: [({ data, }) => {
+    beforeChange: [async ({ data, req }) => {
+      if (!isAdmin({ req })) data = { to: data.to, from: req.user.id }
+
+      if (!data.to) throw new Error("to field not found")
       if (data.to === data.from) throw new Error("Can't follow yourself idiot")
+
+      const follow = await req.payload.collections["follows"].Model.findOne(data)
+      if (follow) throw new Error("Already followed!")
+
+      return data
     }]
-  }
+  },
+  endpoints: [
+    {
+      path: "/unfollow",
+      method: "post",
+      handler: [async (req, res, next) => {
+        const to = req.body?.to
+        const from = req.user?.id
+
+        if (!from) return next(new Error("not authenticated"))
+        if (!to) return next(new Error("to field not found"))
+
+        await req.payload.collections["follows"].Model.deleteOne({ to, from })
+        res.status(200).end()
+      }]
+    }
+  ]
 }
 
 export default Follows
