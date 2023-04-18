b52f42456781adc6a0f147fdf7fd02be0c38b19e
DireWolf707 <rahulsood707@gmail.com>
2023-04-19 01:20:10 +0530

    added feed api

--- src/collections/Posts.ts
+++ src/collections/Posts.ts
@@ -68,6 +68,63 @@ const Posts: CollectionConfig = {
       }
     }],
   },
+  endpoints: [{
+    path: "/feed",
+    method: "get",
+    handler: async (req, res, next) => {
+      const userId = req.user?.id
+      if (!userId) return next(new Error("not authenticated"))
+
+      const feed = await req.payload.collections["follows"].Model.aggregate([
+        { $match: { from: userId } },
+        { $project: { to: 1, _id: 0 } },
+        {
+          $lookup: {
+            from: "posts",
+            as: "posts",
+            let: { requiredUser: "$to" },
+            pipeline: [
+              { $match: { $expr: { $eq: ["$userId", "$$requiredUser"] } } },
+              { $project: { userId: 1, caption: 1, createdAt: 1, url: 1 } },
+              {
+                $lookup: {
+                  from: "likes",
+                  as: "likes",
+                  let: { requiredPost: "$_id" },
+                  pipeline: [
+                    { $set: { _requiredPost: { $toString: "$$requiredPost" } } },
+                    { $match: { $expr: { $eq: ["$postId", "$_requiredPost"] } } },
+                  ]
+                }
+              },
+              { $set: { totalLikes: { $size: "$likes" }, liked: { $in: [userId, "$likes.userId"] } } },
+              { $unset: "likes" },
+              {
+                $lookup: {
+                  from: "users",
+                  as: "user",
+                  let: { requiredUser: "$userId" },
+                  pipeline: [
+                    { $set: { _requiredUser: { $toObjectId: "$$requiredUser" } } },
+                    { $match: { $expr: { $eq: ["$_id", "$_requiredUser"] } } },
+                  ]
+                }
+              },
+              { $unwind: "$user" },
+              { $set: { username: "$user.username" } },
+              { $unset: "user" },
+            ]
+          }
+        },
+        { $unset: "to" },
+        { $unwind: "$posts" },
+        { $replaceRoot: { newRoot: "$posts" } },
+        { $sort: { createdAt: -1 } }
+      ])
+
+      res.status(200).json({ feed })
+    }
+  }]
 }
 
 export default Posts
