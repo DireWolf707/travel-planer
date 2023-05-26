4668c5d3bc31a33aa8255587d9c0f19c4cb70f87
DireWolf707 <rahulsood707@gmail.com>
2023-05-26 21:50:49 +0530

    added profile endpoint

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,5 +1,7 @@
 import { CollectionConfig } from "payload/types"
 import { isAnyone, isAdminOrSelf, isAuthenticated } from "../access"
+import { profilePipeline } from "../utils/pipelines"
+import { Types } from "mongoose"
 
 const Users: CollectionConfig = {
   slug: "users",
@@ -28,6 +30,32 @@ const Users: CollectionConfig = {
       unique: true,
       required: true,
       saveToJWT: true,
+    },
+  ],
+  endpoints: [
+    {
+      path: "/profile/:profileId",
+      method: "get",
+      handler: async (req, res, next) => {
+        const userId = req.user?.id
+        if (!userId) return next(new Error("not authenticated"))
+
+        const { profileId } = req.params
+        // @ts-ignore
+        const [profile] = await req.payload.collections["users"].Model.aggregate(profilePipeline(Types.ObjectId(profileId), userId))
+        const {
+          docs: [isFollowed],
+        } = await req.payload.find({
+          collection: "follows",
+          where: {
+            from: userId,
+            // @ts-ignore
+            to: profileId,
+          },
+        })
+        
+        res.status(200).json({ profile, isFollowed: Boolean(isFollowed) })
+      },
     },
   ],
 }
